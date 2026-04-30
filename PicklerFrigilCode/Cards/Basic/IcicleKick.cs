a775735dcd942e13c42d3bca1c71bb9d42eb0847
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-04-30 11:44:51 +0300

    Changed upgrade to increase Flow instead of Damage

--- PicklerFrigilCode/Cards/Basic/IcicleKick.cs
+++ PicklerFrigilCode/Cards/Basic/IcicleKick.cs
@@ -24,7 +24,7 @@ public class IcicleKick() : PicklerFrigilCard(1,
     
     protected override HashSet<CardTag> CanonicalTags => [IcyTag];
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DamageVar(3, ValueProp.Move),
+        new DamageVar(4, ValueProp.Move),
         new PowerVar<FlowPower>(2)
     ];
     
@@ -37,6 +37,6 @@ public class IcicleKick() : PicklerFrigilCard(1,
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(2m);
+        DynamicVars["FlowPower"].UpgradeValueBy(2m);
     }
 }
