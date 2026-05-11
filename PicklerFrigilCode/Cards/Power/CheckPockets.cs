be767573cbcb9030093930882e4ab2e4e654db1d
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-11 15:14:56 +0300

    fixed to properly use PowerVar instead of generic DynamicVar

--- PicklerFrigilCode/Cards/Power/CheckPockets.cs
+++ PicklerFrigilCode/Cards/Power/CheckPockets.cs
@@ -13,18 +13,18 @@ public class CheckPockets() : PicklerFrigilCard(1,
     TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DynamicVar("Gems", 1)
+        new PowerVar<CheckPocketsPower>(1)
     ];
     
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        await PowerCmd.Apply<CheckPocketsPower>(choiceContext, Owner.Creature, DynamicVars["Gems"].BaseValue, Owner.Creature, this);
+        await PowerCmd.Apply<CheckPocketsPower>(choiceContext, Owner.Creature, DynamicVars["CheckPocketsPower"].BaseValue, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["Gems"].UpgradeValueBy(1);
+        DynamicVars["CheckPocketsPower"].UpgradeValueBy(1);
     }
 }
