fb1df7c28299d80bd0b39a925f49b27ef5b29f69
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-11 14:52:13 +0300

    Cleaned comments of old code

--- PicklerFrigilCode/Powers/ColdBloodedPower.cs
+++ PicklerFrigilCode/Powers/ColdBloodedPower.cs
@@ -11,28 +11,11 @@ using PicklerFrigil.PicklerFrigilCode.Powers;
 
 namespace PicklerFrigil.PicklerFrigilCode.Cards.Power;
 
-public class ColdBloodedPower : CustomPowerModel
+public class ColdBloodedPower : PicklerFrigilPower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
     
-    //public override string? CustomPackedIconPath => "res://PicklerFrigil/images/powers/picklerfrigil-hypothermia_power.png";
-    //public override string? CustomBigIconPath => "res://PicklerFrigil/images/powers/big/picklerfrigil-hypothermia_power.png";
-    
-    /*public override async Task BeforeDamageReceived(
-        PlayerChoiceContext choiceContext,
-        Creature target,
-        Decimal amount,
-        ValueProp props,
-        Creature? dealer,
-        CardModel? cardSource)
-    {
-        if (target != Owner || dealer == null || !props.IsPoweredAttack() && !(cardSource is Omnislice))
-            return;
-        Flash();
-        await PowerCmd.Apply<HypothermiaPower>(choiceContext, dealer, Amount, Owner, null);
-        //IEnumerable<DamageResult> damageResults = await CreatureCmd.Damage(choiceContext, dealer, (Decimal) thornsPower.Amount, ValueProp.Unpowered | ValueProp.SkipHurtAnim, thornsPower.Owner, (CardModel) null);
-    }*/
     public override async Task AfterDamageReceived(PlayerChoiceContext choiceContext, Creature target, DamageResult result, ValueProp props,
         Creature? dealer, CardModel? cardSource)
     {
