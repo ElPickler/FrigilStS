da761e672f8427e530c8cb567ee458df750a896f
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-11 14:57:46 +0300

    Reassigned hook to avoid interference with block removal

--- PicklerFrigilCode/Powers/SleetStormPower.cs
+++ PicklerFrigilCode/Powers/SleetStormPower.cs
@@ -9,15 +9,21 @@ using MegaCrit.Sts2.Core.Models;
 namespace PicklerFrigil.PicklerFrigilCode.Powers;
 
 
-public class SleetStormPower : CustomPowerModel
+public class SleetStormPower : PicklerFrigilPower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
     
-    public override string? CustomPackedIconPath => "res://PicklerFrigil/images/powers/picklerfrigil-sleet_storm_power.png";
-    public override string? CustomBigIconPath => "res://PicklerFrigil/images/powers/big/picklerfrigil-sleet_storm_power.png";
-    
-    
+    public override string CustomPackedIconPath => "res://PicklerFrigil/images/powers/picklerfrigil-sleet_storm_power.png";
+    public override string CustomBigIconPath => "res://PicklerFrigil/images/powers/big/picklerfrigil-sleet_storm_power.png";
+
+    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        if (side == CombatSide.Player)
+            await PowerCmd.Apply<HypothermiaPower>(choiceContext, CombatState.HittableEnemies, Amount, Owner, null);
+    }
+
+    /*
     public override async Task BeforeSideTurnStart(PlayerChoiceContext choiceContext, CombatSide side, ICombatState combatState)
     {
         if (side != CombatSide.Player)
@@ -25,5 +31,5 @@ public class SleetStormPower : CustomPowerModel
             await PowerCmd.Apply<HypothermiaPower>(choiceContext, combatState.HittableEnemies, Amount, Owner, null);
         }
     }
-    
+    */
 }
