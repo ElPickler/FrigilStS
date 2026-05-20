d1442dfbd103b4a2b6f148af684cbc4be5335db7
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-20 13:58:25 +0300

    Reworked effect of Hyperthermia. Card no longer prevents Icy from benefitting from Hypothermia, instead the card makes the debuff much harder to apply.

--- PicklerFrigilCode/Powers/HyperthermiaPower.cs
+++ PicklerFrigilCode/Powers/HyperthermiaPower.cs
@@ -1,12 +1,17 @@
 using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
 
 namespace PicklerFrigil.PicklerFrigilCode.Powers;
 
 
 public class HyperthermiaPower : PicklerFrigilPower
 {
+    private const int HypothermiaLoss = 2;
+    
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Single;
     
@@ -14,4 +19,19 @@ public class HyperthermiaPower : PicklerFrigilPower
     {
         return player != Owner.Player ? amount : amount + Amount;
     }
+
+    public override decimal ModifyPowerAmountGiven(PowerModel power, Creature giver, decimal amount, Creature? target,
+        CardModel? cardSource)
+    {
+        if(power is not HypothermiaPower)
+            return base.ModifyPowerAmountGiven(power, giver, amount, target, cardSource);
+        if(giver != Owner)
+            return base.ModifyPowerAmountGiven(power, giver, amount, target, cardSource);
+        if(cardSource is null)
+            return base.ModifyPowerAmountGiven(power, giver, amount, target, cardSource);
+        
+        Flash();
+        return base.ModifyPowerAmountGiven(power, giver, amount - HypothermiaLoss, target, cardSource);
+    }
+    
 }
