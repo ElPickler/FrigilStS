768b43b3a86cc472dc8b7af9f48f35a4974bbd7b
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-21 19:49:11 +0300

    Created and added art for icon

--- PicklerFrigilCode/Powers/MetabolizePower.cs
+++ PicklerFrigilCode/Powers/MetabolizePower.cs
@@ -11,6 +11,9 @@ public class MetabolizePower: PicklerFrigilPower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override string CustomPackedIconPath => "res://PicklerFrigil/images/powers/picklerfrigil-metabolize_power.png";
+    public override string CustomBigIconPath => "res://PicklerFrigil/images/powers/big/picklerfrigil-metabolize_power.png";
 
     public override async Task AfterCardExhausted(PlayerChoiceContext choiceContext, CardModel card, bool causedByEthereal)
     {
