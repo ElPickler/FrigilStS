b6a0c865631f39bcb4d2d532cde76c3c57900819
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-22 07:10:24 +0300

    In addition to being fixed for the latest game version, removed base block gain.

--- PicklerFrigilCode/Powers/MetabolizingDiamondPower.cs
+++ PicklerFrigilCode/Powers/MetabolizingDiamondPower.cs
@@ -1,5 +1,6 @@
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
@@ -15,15 +16,15 @@ public class MetabolizingDiamondPower : PicklerFrigilPower
     
     public override string CustomPackedIconPath => "res://PicklerFrigil/images/powers/picklerfrigil-metabolizing_diamond_power.png";
     public override string CustomBigIconPath => "res://PicklerFrigil/images/powers/big/picklerfrigil-metabolizing_diamond_power.png";
-
-    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    
+    public override async Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
     {
 
         if (side == CombatSide.Enemy)
             return;
-        await CreatureCmd.GainBlock(Owner, Amount, ValueProp.Unpowered, null);
+        //await CreatureCmd.GainBlock(Owner, Amount, ValueProp.Unpowered, null);
         // ReSharper disable once PossibleLossOfFraction
-        await PowerCmd.ModifyAmount(choiceContext, this, Amount / -2 , Owner, null);
+        await PowerCmd.ModifyAmount(choiceContext, this, Amount - Amount/2 , Owner, null);
     }
 
 
