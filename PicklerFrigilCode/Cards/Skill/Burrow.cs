206d2144a4087704d27afdb2334999ad65d62117
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-26 16:28:09 +0300

    Changed card preview style

--- PicklerFrigilCode/Cards/Skill/Burrow.cs
+++ PicklerFrigilCode/Cards/Skill/Burrow.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
 using MegaCrit.Sts2.Core.ValueProps;
 using PicklerFrigil.PicklerFrigilCode.Commands;
 
@@ -31,7 +32,7 @@ public class Burrow() : PicklerFrigilCard(0,
         CardPlay play)
     {
         await CommonActions.CardBlock(this, play);
-        await GemstoneCmd.GenerateGemstone(Owner, (int) DynamicVars["Gems"].BaseValue, PileType.Discard);
+        await GemstoneCmd.GenerateGemstone(Owner, (int) DynamicVars["Gems"].BaseValue, PileType.Discard, CardPreviewStyle.MessyLayout);
     }
 
     protected override void OnUpgrade()
