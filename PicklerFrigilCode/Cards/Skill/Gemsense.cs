efcf210173f86836586cb2e0f6ee3ec6f9fa5158
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-05-26 16:29:28 +0300

    Gensense reworked, now exhausts gemstones from draw pile and adds more to draw pile.

--- PicklerFrigilCode/Cards/Skill/Gemsense.cs
+++ PicklerFrigilCode/Cards/Skill/Gemsense.cs
@@ -1,8 +1,11 @@
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using PicklerFrigil.PicklerFrigilCode.Cards;
+using MegaCrit.Sts2.Core.Models;
 using PicklerFrigil.PicklerFrigilCode.Commands;
 
 namespace PicklerFrigil.PicklerFrigilCode.Cards.Skill;
@@ -21,20 +24,33 @@ public class Gemsense() : PicklerFrigilCard(1,
     }
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new ("Gems", 3)
+        new ("Gems", 2),
+        new ("Exhaust", 3)
     ];
     
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    //public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
     
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        await GemstoneCmd.GenerateGemstone(Owner, (int) DynamicVars["Gems"].BaseValue);
+        CardSelectorPrefs prefs = new CardSelectorPrefs(CardSelectorPrefs.ExhaustSelectionPrompt, 0, DynamicVars["Exhaust"].IntValue);
+        List<CardModel> select = (await CardSelectCmd.FromCombatPile(choiceContext, PileType.Draw.GetPile(Owner), Owner, prefs, Filter)).ToList();
+        foreach (CardModel gem in select)
+        {
+            await CardCmd.Exhaust(choiceContext, gem);
+        }
+        
+        bool Filter(CardModel c)
+        {
+            return c.Tags.Contains(GemTag);
+        }
+        
+        await GemstoneCmd.GenerateGemstone(Owner, (int) DynamicVars["Gems"].BaseValue, PileType.Draw);
     }
 
     protected override void OnUpgrade()
     {
-        EnergyCost.UpgradeBy(-1);
+        DynamicVars["Exhaust"].UpgradeValueBy(1);
     }
 }
