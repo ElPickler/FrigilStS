799e9059606704952d2c10a609f4f315b7461fc9
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-06-15 13:02:52 +0300

    Added support for AutoModAudio and Godot Scripts

--- MainFile.cs
+++ MainFile.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using BaseLib.Audio;
 using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Modding;
@@ -8,14 +10,18 @@ namespace PicklerFrigil;
 public partial class MainFile : Node
 {
     public const string ModId = "PicklerFrigil"; //Used for resource filepath
+    
+    public static readonly AutoModAudio Audio = new AutoModAudio($"res://PicklerFrigil/audio/");
 
     public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
         new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
-
+    
     public static void Initialize()
     {
         Harmony harmony = new(ModId);
 
         harmony.PatchAll();
+        
+        Godot.Bridge.ScriptManagerBridge.LookupScriptsInAssembly(Assembly.GetExecutingAssembly());
     }
 }
