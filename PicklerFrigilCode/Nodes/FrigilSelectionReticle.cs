98d4783f0dd41b810702789fabc1df486f7837f9
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-06-15 13:05:19 +0300

    Added scripts to Rest Site

---
+++ PicklerFrigilCode/Nodes/FrigilSelectionReticle.cs
@@ -0,0 +1,11 @@
+#region
+
+using MegaCrit.Sts2.Core.Nodes.Combat;
+
+#endregion
+
+namespace PicklerFrigil.PicklerFrigilCode.Nodes;
+
+public partial class FrigilSelectionReticle : NSelectionReticle
+{
+} 
