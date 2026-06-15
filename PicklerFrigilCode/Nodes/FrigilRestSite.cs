ddcd89e0f74d16e195af78f1b2d773453afc7f43
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-06-15 18:07:32 +0300

    Removed unused code

--- PicklerFrigilCode/Nodes/FrigilRestSite.cs
+++ PicklerFrigilCode/Nodes/FrigilRestSite.cs
@@ -9,37 +9,5 @@ namespace PicklerFrigil.PicklerFrigilCode.Nodes;
 
 public partial class FrigilRestSite : NRestSiteCharacter
 {
-	/*
-	public void FlipX()
-	{
-		
-		Vector2 scale;
-		foreach (Node2D childSpineNode in GetChildSpineNodes())
-		{
-			scale = childSpineNode.Scale;
-			scale.X = 0f - childSpineNode.Scale.X;
-			childSpineNode.Scale = scale;
-			scale = childSpineNode.Position;
-			scale.X = 0f - childSpineNode.Position.X;
-			childSpineNode.Position = scale;
-		}
-		Control controlRoot = _controlRoot;
-		scale = _controlRoot.Scale;
-		scale.X = 0f - _controlRoot.Scale.X;
-		controlRoot.Scale = scale;
-		
-		
-		Sprite2D sprite = GetNode<Sprite2D>("%Sprite2D");
-		sprite.FlipH = true;
-		
-		
-		
-		Vector2 scale;
-		Control root = GetNode<Control>("ControlRoot");
-		Control controlRoot = root;
-		scale = root.Scale;
-		scale.X = 0f - root.Scale.X;
-		controlRoot.Scale = scale;
-		
-	}*/
+	
 }
