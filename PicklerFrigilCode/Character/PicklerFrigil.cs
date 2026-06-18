bc5a313eeaeff0d334db19ff7b30a2e222ad11b4
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-06-18 14:52:20 +0300

    Added custom energy marker. Added outline color for energy counter text. Reorganized entire structure to be more readable.

--- PicklerFrigilCode/Character/PicklerFrigil.cs
+++ PicklerFrigilCode/Character/PicklerFrigil.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.InteropServices;
 using BaseLib.Abstracts;
-using BaseLib.Audio;
 using BaseLib.Patches.UI;
 using BaseLib.Utils.NodeFactories;
 using PicklerFrigil.PicklerFrigilCode.Extensions;
@@ -8,7 +7,6 @@ using Godot;
 using MegaCrit.Sts2.Core.Entities.Characters;
 using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Models;
-using PicklerFrigil.Cards;
 using PicklerFrigil.Cards.Basic;
 using PicklerFrigil.PicklerFrigilCode.Cards.Basic;
 using PicklerFrigil.PicklerFrigilCode.Relics;
@@ -19,13 +17,18 @@ namespace PicklerFrigil.PicklerFrigilCode.Character;
 public class PicklerFrigil : CustomCharacterModel
 {
     public const string CharacterId = "PicklerFrigil";
+    public virtual string PlaceholderID => "ironclad";
 
+    //Colors
     public static readonly Color Color = new("a7e7eb");
-
     public override Color NameColor => Color;
+    public override Color EnergyLabelOutlineColor => new("380d4d");
+    public override Color MapDrawingColor => new ("482675");
+    
     public override CharacterGender Gender => CharacterGender.Masculine;
-    public override int StartingHp => 72;
 
+    //Run Start Properties
+    public override int StartingHp => 72;
     public override IEnumerable<CardModel> StartingDeck =>
     [
         ModelDb.Card<StrikeFrigil>(), 
@@ -39,47 +42,45 @@ public class PicklerFrigil : CustomCharacterModel
         ModelDb.Card<IcicleKick>(),
         ModelDb.Card<DeepChill>()
     ];
-
     public override IReadOnlyList<RelicModel> StartingRelics =>
     [
         ModelDb.Relic<GlisteningAmethyst>()
     ];
 
+    //Animation Delays
     public override float AttackAnimDelay => 0.2f;
     public override float CastAnimDelay => 0.35f;
 
+    //Pools
     public override CardPoolModel CardPool => ModelDb.CardPool<PicklerFrigilCardPool>();
     public override RelicPoolModel RelicPool => ModelDb.RelicPool<PicklerFrigilRelicPool>();
     public override PotionPoolModel PotionPool => ModelDb.PotionPool<PicklerFrigilPotionPool>();
         
-    public override Color MapDrawingColor => new ("482675");
-    
-
-    //Already replaced
-    
+    //Textures
+        //Generic
     public override string CustomIconTexturePath => "res://PicklerFrigil/images/charui/character_icon_frigil2.png";
     public override string CustomCharacterSelectIconPath => "char_select_frigil.png".CharacterUiPath();
     public override string CustomCharacterSelectLockedIconPath => "char_select_char_name_locked.png".CharacterUiPath();
     public override string CustomMapMarkerPath => "map_marker_frigil.png".CharacterUiPath();
 
+        //Chest Screen
     public override string CustomArmPointingTexturePath => "res://PicklerFrigil/images/character/ui/hand_point.png";
     public override string CustomArmRockTexturePath => "res://PicklerFrigil/images/character/ui/hand_rock.png";
     public override string CustomArmPaperTexturePath => "res://PicklerFrigil/images/character/ui/hand_paper.png";
     public override string CustomArmScissorsTexturePath => "res://PicklerFrigil/images/character/ui/hand_scissors.png";
-    
-    public override string CustomVisualPath => "res://PicklerFrigil/scenes/frigil.tscn";
-    public override string CustomCharacterSelectBg => "res://PicklerFrigil/scenes/CharSelect/char_select_bg_frigil.tscn";
-    
-    public override string CustomCharacterSelectTransitionPath => "res://PicklerFrigil/materials/frigil_transition_mat.tres";
-
     public override RelicIconData CustomYummyCookie => new (
         "C:/Users/aleja/RiderProjects/PicklerFrigil/PicklerFrigil/images/relics/big/yummy_cookie_frigil.png",
         "C:/Users/aleja/RiderProjects/PicklerFrigil/PicklerFrigil/images/relics/yummy_cookie_frigil.png",
         "C:/Users/aleja/RiderProjects/PicklerFrigil/PicklerFrigil/images/relics/yummy_cookie_frigil_outline.png"
     );
     
+    //Scenes
+    public override string CustomVisualPath => "res://PicklerFrigil/scenes/frigil.tscn";
+    public override string CustomCharacterSelectBg => "res://PicklerFrigil/scenes/CharSelect/char_select_bg_frigil.tscn";
+    public override string CustomCharacterSelectTransitionPath => "res://PicklerFrigil/materials/frigil_transition_mat.tres";
+    public override string CustomEnergyCounterPath => "res://PicklerFrigil/scenes/ui/FrigilEnergyCounter.tscn";
     public override string CustomRestSiteAnimPath => "res://PicklerFrigil/scenes/RestSite/frigilRestSite2.tscn";
-    
+    public override string CustomMerchantAnimPath => "res://PicklerFrigil/scenes/Merchant/frigilmerchant.tscn";
     public override Control CustomIcon
     {
         get
@@ -89,35 +90,22 @@ public class PicklerFrigil : CustomCharacterModel
             return icon;
         }
     }
-    
-    public override string CustomMerchantAnimPath => "res://PicklerFrigil/scenes/Merchant/frigilmerchant.tscn";
-    
-    
-    
-        //audio
-    public override string CharacterSelectSfx => "res://PicklerFrigil/audio/FrigilCharSelect.ogg";
-    public override string CharacterTransitionSfx => "res://PicklerFrigil/audio/FrigilTransition.ogg";
-
-    
-    
-    //To be replaced
-    public virtual string PlaceholderID => "ironclad";
-    
+        //PLACEHOLDER
     public override string CustomTrailPath
     {
         get => SceneHelper.GetScenePath("vfx/card_trail_" + this.PlaceholderID);
     }
     
-    public override string CustomEnergyCounterPath
-    {
-        get => SceneHelper.GetScenePath($"combat/energy_counters/{this.PlaceholderID}_energy_counter");
-    }
     
-    public override string CustomIconPath
-    {
-        get => SceneHelper.GetScenePath($"ui/character_icons/{this.PlaceholderID}_icon");
-    }
+    //Audio
+    public override string CharacterSelectSfx => "res://PicklerFrigil/audio/FrigilCharSelect.ogg";
+    public override string CharacterTransitionSfx => "res://PicklerFrigil/audio/FrigilTransition.ogg";
     
+        //PLACEHOLDER
+    public override string CustomAttackSfx
+    {
+        get => $"event:/sfx/characters/{this.PlaceholderID}/{this.PlaceholderID}_attack";
+    }
     public override string CustomCastSfx
     {
         get => $"event:/sfx/characters/{this.PlaceholderID}/{this.PlaceholderID}_cast";
@@ -127,12 +115,9 @@ public class PicklerFrigil : CustomCharacterModel
     {
         get => $"event:/sfx/characters/{this.PlaceholderID}/{this.PlaceholderID}_die";
     }
-    
-    public override string CustomAttackSfx
-    {
-        get => $"event:/sfx/characters/{this.PlaceholderID}/{this.PlaceholderID}_attack";
-    }
-    
+
+    //Special
+
     public override List<string> GetArchitectAttackVfx()
     {
         int num = 5;
@@ -151,4 +136,11 @@ public class PicklerFrigil : CustomCharacterModel
         span[index5] = "vfx/vfx_big_slash_impact";
         return list;
     }
+    
+   /* 
+    public override string CustomIconPath
+    {
+        get => SceneHelper.GetScenePath($"ui/character_icons/{this.PlaceholderID}_icon");
+    }
+    */
 }
