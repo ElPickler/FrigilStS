1be7cd0fbd6deffa59efd795b9667738c31c6d94
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-06-24 00:30:18 +0300

    Added ModConfig

--- FrigilMainFile.cs
+++ FrigilMainFile.cs
@@ -1,8 +1,11 @@
 using System.Reflection;
 using BaseLib.Audio;
+using BaseLib.Config;
 using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Modding;
+using MegaCrit.Sts2.Core.Models;
+using PicklerFrigil.PicklerFrigilCode;
 
 namespace PicklerFrigil;
 
@@ -11,15 +14,16 @@ public partial class FrigilMainFile : Node
 {
     public const string ModId = "PicklerFrigil"; //Used for resource filepath
     
-    public static readonly AutoModAudio Audio = new AutoModAudio($"res://PicklerFrigil/audio/");
+    //public static readonly AutoModAudio Audio = new AutoModAudio($"res://PicklerFrigil/audio/");
 
     public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
         new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
     
     public static void Initialize()
     {
+        ModConfigRegistry.Register(ModId, new FrigilModConfig());
+       
         Harmony harmony = new(ModId);
-
         harmony.PatchAll();
         
         Godot.Bridge.ScriptManagerBridge.LookupScriptsInAssembly(Assembly.GetExecutingAssembly());
---
+++ PicklerFrigilCode/FrigilModConfig.cs
@@ -0,0 +1,8 @@
+using BaseLib.Config;
+
+namespace PicklerFrigil.PicklerFrigilCode;
+
+public class FrigilModConfig:SimpleModConfig
+{
+    public static bool BetaArt { get; set; } = false;
+}
