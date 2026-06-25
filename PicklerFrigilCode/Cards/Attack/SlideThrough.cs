bae2ddc09a9bcfd57b11a5dd638033965cf4079c
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-06-25 21:36:59 +0300

    Reworked Slide Through entirely.

--- PicklerFrigilCode/Cards/Attack/SlideThrough.cs
+++ PicklerFrigilCode/Cards/Attack/SlideThrough.cs
@@ -1,32 +1,35 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
-using PicklerFrigil.PicklerFrigilCode.Cards;
 using PicklerFrigil.PicklerFrigilCode.Powers;
 
 namespace PicklerFrigil.PicklerFrigilCode.Cards.Attack;
 
 
 public class SlideThrough() : PicklerFrigilCard(1,
-    CardType.Attack, CardRarity.Common,
+    CardType.Attack, CardRarity.Uncommon,
     TargetType.AllEnemies)
 
 {
     protected override IEnumerable<IHoverTip> ExtraHoverTips
     {
         get { 
+            yield return HoverTipFactory.FromPower<VulnerablePower>();
             yield return HoverTipFactory.FromPower<FlowPower>();
         }
     }
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DamageVar(4, ValueProp.Move),
+        new DamageVar(6, ValueProp.Move),
         new PowerVar<FlowPower>(2M),
-        new CardsVar(1)
+        new PowerVar<VulnerablePower>(1)
     ];
     
     protected override async Task OnPlay(
@@ -34,13 +37,27 @@ public class SlideThrough() : PicklerFrigilCard(1,
         CardPlay play)
     {
         await CommonActions.CardAttack(this, play.Target).Execute(choiceContext);
-        await PowerCmd.Apply<FlowPower>(choiceContext, Owner.Creature, DynamicVars["FlowPower"].BaseValue, Owner.Creature, this, false);
-        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+    }
+
+    public override async Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer, DamageResult result, ValueProp props,
+        Creature target, CardModel? cardSource)
+    {
+        if (cardSource != this)
+            return;
+        
+        if (result.WasFullyBlocked)
+        {
+            await PowerCmd.Apply<VulnerablePower>(choiceContext, dealer!, DynamicVars["VulnerablePower"].BaseValue, dealer, this);
+            return;
+        }
+        
+        await PowerCmd.Apply<FlowPower>(choiceContext, dealer!, DynamicVars["FlowPower"].BaseValue, dealer, this);
+        await PowerCmd.Apply<VulnerablePower>(choiceContext, target,  DynamicVars["VulnerablePower"].BaseValue, dealer, this);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(2m);
+        DynamicVars.Damage.UpgradeValueBy(3m);
         DynamicVars["FlowPower"].UpgradeValueBy(1m);
     }
 }
