d87603f4c0ca91b32cba628c4fd0e6b21eae319e
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-07-02 18:13:07 +0300

    Added Beta Card art toggle

--- PicklerFrigilCode/Cards/PicklerFrigilCard.cs
+++ PicklerFrigilCode/Cards/PicklerFrigilCard.cs
@@ -2,6 +2,7 @@
 using BaseLib.Extensions;
 using BaseLib.Patches.Content;
 using BaseLib.Utils;
+using Godot;
 using PicklerFrigil.PicklerFrigilCode.Character;
 using PicklerFrigil.PicklerFrigilCode.Extensions;
 using MegaCrit.Sts2.Core.Entities.Cards;
@@ -25,13 +26,51 @@ public abstract class PicklerFrigilCard(int cost, CardType type, CardRarity rari
     //Image size:
     //Normal art: 1000x760 (Using 500x380 should also work, it will simply be scaled.)
     //Full art: 606x852
-    public override string CustomPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
-
+    
+    //public override string CustomPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+    public override string CustomPortraitPath => GetBigPortraitPath();
+    
+    private string GetBigPortraitPath()
+    {
+        string path;
+        if (FrigilModConfig.BetaCards)
+        {
+             path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_beta.png".BigCardImagePath();
+             if (ResourceLoader.Exists(path))
+             {
+                 return path;
+             }
+                 
+        }
+        return $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+    }
+    
     //Smaller variants of card images for efficiency:
     //Smaller variant of fullart: 250x350
     //Smaller variant of normalart: 250x190
 
     //Uses card_portraits/card_name.png as image path. These should be smaller images.
-    public override string PortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
-    public override string BetaPortraitPath => $"beta/{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+    
+    
+    
+    public override string PortraitPath => GetSmallPortraitPath();
+    
+    
+    private string GetSmallPortraitPath()
+    {
+        string path;
+        if (FrigilModConfig.BetaCards)
+        {
+            path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_beta.png".CardImagePath();
+            if (ResourceLoader.Exists(path))
+            {
+                return path;
+            }
+                 
+        }
+        return $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+    }
+    
+    
+    //public override string BetaPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_beta.png".CardImagePath();
 }
--- PicklerFrigilCode/FrigilModConfig.cs
+++ PicklerFrigilCode/FrigilModConfig.cs
@@ -5,4 +5,5 @@ namespace PicklerFrigil.PicklerFrigilCode;
 public class FrigilModConfig:SimpleModConfig
 {
     public static bool BetaArt { get; set; } = false;
+    public static bool BetaCards { get; set; } = false;
 }
