5fe1d8574394cb960f3c235bd9528a7263b68ee8
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-07-03 09:32:55 +0300

    New cards

---
+++ PicklerFrigilCode/Cards/Attack/ShimmeringCyclone.cs
@@ -0,0 +1,49 @@
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using PicklerFrigil.PicklerFrigilCode.Cards;
+
+namespace PicklerFrigil.PicklerFrigilCode.Cards.Attack;
+
+
+public class ShimmeringCyclone() : PicklerFrigilCard(1,
+    CardType.Attack, CardRarity.Uncommon,
+    TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new DamageVar(4, ValueProp.Move),
+        new CalculationBaseVar(0M),
+        new CalculationExtraVar(1M),
+        new CalculatedVar("CalculatedHits").WithMultiplier(static (card, target) => GetGemstones(card.Owner))
+
+    ];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).WithHitCount((int) ((CalculatedVar) DynamicVars["CalculatedHits"]).Calculate(play.Target)).FromCard(this, play).Targeting(play.Target!).Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1);
+    }
+
+    private static int GetGemstones(Player player)
+    {
+        
+        return CombatManager.Instance.History.Entries.OfType<CardGeneratedEntry>().Count(
+            (Func<CardGeneratedEntry, bool>)(e => e.Card.Owner == player && e.Card.Tags.Contains(GemTag)));
+    }
+}
