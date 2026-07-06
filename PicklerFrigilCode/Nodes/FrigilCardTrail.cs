b118ca009c1d7cf7d3d3949b232823a9a50f1432
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-07-06 18:26:32 +0300

    Added assets

---
+++ PicklerFrigilCode/Nodes/FrigilCardTrail.cs
@@ -0,0 +1,7 @@
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+namespace PicklerFrigil.PicklerFrigilCode.Nodes;
+
+public partial class FrigilCardTrail : NCardTrail
+{
+}
---
+++ PicklerFrigilCode/Nodes/FrigilCardTrailNode.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
+
+public partial class FrigilCardTrailNode : NCardTrailVfx
+{
+}
