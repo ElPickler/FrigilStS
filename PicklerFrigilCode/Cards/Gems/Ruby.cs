228bce4e4f18d784cdf05005795ef2b1afba8124
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-05 20:29:58 +0300

    New gemstone, applies Vigor

---
+++ PicklerFrigilCode/Cards/Gems/Ruby.cs
@@ -0,0 +1,39 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PicklerFrigil.PicklerFrigilCode.Cards.Special;
+
+namespace PicklerFrigil.PicklerFrigilCode.Cards.Gems;
+
+
+public class Ruby() : AbstractGem(-1,
+    CardType.Status, CardRarity.Token,
+    TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<VigorPower>( 8M),
+    ];
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+    }
+
+    public override async Task AfterCardExhausted(PlayerChoiceContext choiceContext, CardModel card,
+        bool causedByEthereal)
+    {
+        if (card == this)
+        {
+            await PowerCmd.Apply<VigorPower>(choiceContext, Owner.Creature, DynamicVars["VigorPower"].BaseValue, Owner.Creature, this);
+        }
+    }
+    
+    protected override void OnUpgrade()
+    {
+        DynamicVars["VigorPower"].UpgradeValueBy(5);
+    }
+}
