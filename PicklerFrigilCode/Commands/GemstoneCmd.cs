c6cfbd19c45a9b9b8542a1e65bd7a6e53f44c1af
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-05 20:27:58 +0300

    A lot changed. Rewrote a lot of code to make it more flexible. No longer grants relic directly, but rather gives a card that creates it on exhaust for clarity.

--- PicklerFrigilCode/Commands/GemstoneCmd.cs
+++ PicklerFrigilCode/Commands/GemstoneCmd.cs
@@ -1,3 +1,4 @@
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Players;
@@ -13,11 +14,18 @@ namespace PicklerFrigil.PicklerFrigilCode.Commands;
 
 public class GemstoneCmd
 {
+    private const int SaltOdds = 50; //1 in X chance of getting salt
+    
+    public class GemstoneField
+    {
+        public static readonly SpireField<PlayerCombatState, bool> CreatedSaltThisCombat = new(() => false);
+    }
+    
     public static async Task GenerateGemstone(
         Player player, int count, PileType pileType = PileType.Hand, CardPreviewStyle previewStyle = CardPreviewStyle.HorizontalLayout)
     {
         int moddedCount = count;
-        
+        IEnumerable<CardModel> c = new List<CardModel>();
         
         if (player.Creature.HasPower<RubyBloodPower>())
         {
@@ -25,20 +33,19 @@ public class GemstoneCmd
             player.Creature.GetPower<RubyBloodPower>()!.InvokeFlash();
         }
 
-        CrystallizedSalt? salt = player.GetRelic<CrystallizedSalt>();
-        if (salt == null) 
+        
+        if (TrySalt(player))
         {
-            int rng = player.RunState.Rng.Niche.NextInt(0, 50); // 2% chance to get salt IF you don't already have it
-            if (rng == 0)
-            {
-                moddedCount--;
-                await RelicCmd.Obtain<CrystallizedSalt>(player);
-                player.GetRelic<CrystallizedSalt>()!.InvokeFlash();
-            }
+            CardModel salt = player.Creature.CombatState!.CreateCard<SaltRock>(player);
+            c = c.Append(salt);
+            moddedCount--;
         }
         
         
-        IEnumerable<CardModel> c = CardFactory.GetDistinctForCombat(player, Gems, moddedCount, player.RunState.Rng.Shuffle);
+        IEnumerable<CardModel> generatedGems = new List<CardModel>(CardFactory.GetDistinctForCombat(player, Gems, moddedCount, player.RunState.Rng.CombatCardGeneration));
+        foreach (CardModel card in generatedGems)
+            c = c.Append(card);
+        
         
         if(pileType == PileType.Draw)
             CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardsToCombat(c, pileType, player, CardPilePosition.Random), 0.4F, previewStyle);
@@ -48,11 +55,34 @@ public class GemstoneCmd
 
     private static readonly AbstractGem[] Gems =
     {
-        ModelDb.Card<Topaz>(),
         ModelDb.Card<Amethyst>(),
         ModelDb.Card<Diamond>(),
         ModelDb.Card<Opal>(),
         ModelDb.Card<Emerald>(),
-        ModelDb.Card<Quartz>()
+        ModelDb.Card<Quartz>(),
+        ModelDb.Card<Serpentine>(),
+        ModelDb.Card<Ruby>()
     };
+
+    private static bool TrySalt(Player player, PileType pileType = PileType.Hand, CardPreviewStyle previewStyle = CardPreviewStyle.HorizontalLayout)
+    {
+        CrystallizedSalt? salt = player.GetRelic<CrystallizedSalt>();
+        if (salt == null) 
+        {
+            if (GemstoneField.CreatedSaltThisCombat.Get(player.PlayerCombatState!))
+            {
+                return false;
+            }
+            
+            int rng = player.RunState.Rng.Niche.NextInt(0, SaltOdds - 1);
+            if (rng == 0)
+            {
+                GemstoneField.CreatedSaltThisCombat.Set(player.PlayerCombatState!, true);
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
 }
