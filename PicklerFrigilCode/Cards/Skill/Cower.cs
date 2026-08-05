b0330d14f70e74f17a9057312a4823e38ac2e741
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-05 20:24:23 +0300

    New multiplayer card.

---
+++ PicklerFrigilCode/Cards/Skill/Cower.cs
@@ -0,0 +1,48 @@
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace PicklerFrigil.PicklerFrigilCode.Cards.Skill;
+
+
+public class Cower() : PicklerFrigilCard(1,
+    CardType.Skill, CardRarity.Uncommon,
+    TargetType.AnyAlly)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new PowerVar<WeakPower>( 1M),
+        new PowerVar<VulnerablePower>( 1M)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips
+    {
+        get
+        {
+            yield return HoverTipFactory.FromPower < WeakPower>();
+            yield return HoverTipFactory.FromPower < VulnerablePower>();
+        }
+    }
+    
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        foreach (Creature enemy in CombatState!.HittableEnemies)
+        {
+            await PowerCmd.Apply<WeakPower>(choiceContext, enemy, DynamicVars["WeakPower"].BaseValue, Owner.Creature,
+                this);
+        }
+
+        await PowerCmd.Apply<VulnerablePower>(choiceContext, CurrentTarget!, DynamicVars["VulnerablePower"].BaseValue,
+            Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
