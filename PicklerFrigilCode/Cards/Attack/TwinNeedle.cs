e4bece4a5fd83a331f1d2fd84c0773a313d5646f
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-11 16:01:03 +0300

    Hypothermia applivation increased and moved to afterdamagegiven hook

--- PicklerFrigilCode/Cards/Attack/TwinNeedle.cs
+++ PicklerFrigilCode/Cards/Attack/TwinNeedle.cs
@@ -1,8 +1,11 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using PicklerFrigil.PicklerFrigilCode.Powers;
 
@@ -30,16 +33,22 @@ public class TwinNeedle() : PicklerFrigilCard(1,
         new PowerVar<HypothermiaPower>(1)
     ];
     
-    protected override async Task OnPlay(MegaCrit.Sts2.Core.GameActions.Multiplayer.PlayerChoiceContext choiceContext, CardPlay play)
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
         await CommonActions.CardAttack(this, play.Target).WithHitCount(DynamicVars["Repeat"].IntValue).Execute(choiceContext);
-        
-        if(play.Target != null)
-            await PowerCmd.Apply<HypothermiaPower>(choiceContext, play.Target, DynamicVars["HypothermiaPower"].BaseValue, Owner.Creature, this);
+    }
+    
+    public override async Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer, DamageResult result, ValueProp props,
+        Creature target, CardModel? cardSource)
+    {
+        if (cardSource != this)
+            return;
+        await PowerCmd.Apply<HypothermiaPower>(choiceContext, target, DynamicVars["HypothermiaPower"].BaseValue, Owner.Creature, this, false);
     }
     
     protected override void OnUpgrade()
     {
         DynamicVars.Damage.UpgradeValueBy(1m);
+        DynamicVars["HypothermiaPower"].UpgradeValueBy(1m);
     }
 }
