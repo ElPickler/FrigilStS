9885b5d1b9a9aa25487b5220c415125bd70fff8a
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-11 16:08:36 +0300

    Effect reworked, now doubled first Hypothermia application.

--- PicklerFrigilCode/Relics/GlisteningAmethyst.cs
+++ PicklerFrigilCode/Relics/GlisteningAmethyst.cs
@@ -1,14 +1,11 @@
-using System.Buffers;
 using BaseLib.Utils;
-using MegaCrit.Sts2.Core.Combat;
-using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
-using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Rooms;
 using PicklerFrigil.PicklerFrigilCode.Character;
 using PicklerFrigil.PicklerFrigilCode.Powers;
 
@@ -20,27 +17,52 @@ public class GlisteningAmethyst : PicklerFrigilRelic
     public override RelicRarity Rarity =>
         RelicRarity.Starter;
     
-    protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new PowerVar<HypothermiaPower>( 1M)
-    ];
-    
     protected override IEnumerable<IHoverTip> ExtraHoverTips
     {
         get
         {
-            yield return HoverTipFactory.FromPower < HypothermiaPower>();
+            yield return HoverTipFactory.FromPower <HypothermiaPower>();
         }
     }
-
-    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    
+    public override Task AfterRoomEntered(AbstractRoom room)
     {
-        if (Owner != player)
+        if (!(room is CombatRoom))
+            return Task.CompletedTask;
+
+        Status = RelicStatus.Normal;
+        return Task.CompletedTask;
+    }
+
+    
+    public override decimal ModifyPowerAmountGivenMultiplicative(PowerModel power, Creature giver, decimal amount, Creature? target,
+        CardModel? cardSource) //One function to multiply the power amount when calculating the hypothermia amount
+    {
+        FrigilMainFile.Logger.Info("Applying "+ power + " to " + target + " from " + cardSource);
+
+        if (target == null)
+            return 1;
+        if (Status != RelicStatus.Normal)
+            return 1;
+        if (giver != Owner.Creature)
+            return 1;
+        if (power is not HypothermiaPower)
+            return 1;
+
+        FrigilMainFile.Logger.Info("Applied "+ amount * 2 + " Hypothermia to " + target + ". Disabling Relic.");
+        return 2;
+    }
+
+    public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier,
+        CardModel? cardSource) //And another to disable the relic
+    {
+        if (Status != RelicStatus.Normal)
+            return;
+        if (applier != Owner.Creature)
+            return;
+        if (power is not HypothermiaPower)
             return;
         
-        Flash();
-        Creature? enemy = Owner.RunState.Rng.CombatTargets.NextItem(player.Creature.CombatState!.HittableEnemies);
-        if (enemy != null) await PowerCmd.Apply<HypothermiaPower>(choiceContext, enemy, DynamicVars["HypothermiaPower"].BaseValue, Owner.Creature, null); 
-        
-        
+        Status = RelicStatus.Disabled;
     }
 }
