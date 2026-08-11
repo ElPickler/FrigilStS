c55bf8d1f8d873c97ba045afcdded1f409183039
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-11 15:54:28 +0300

    Hypothermia up

--- PicklerFrigilCode/Cards/Skill/LetItGo.cs
+++ PicklerFrigilCode/Cards/Skill/LetItGo.cs
@@ -24,7 +24,7 @@ public class LetItGo() : PicklerFrigilCard(2,
     }
     
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new PowerVar<HypothermiaPower>( 2M)
+        new PowerVar<HypothermiaPower>( 4M)
     ];
     
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
@@ -47,6 +47,6 @@ public class LetItGo() : PicklerFrigilCard(2,
 
     protected override void OnUpgrade()
     {
-        DynamicVars["HypothermiaPower"].UpgradeValueBy(1);
+        DynamicVars["HypothermiaPower"].UpgradeValueBy(2);
     }
 }
