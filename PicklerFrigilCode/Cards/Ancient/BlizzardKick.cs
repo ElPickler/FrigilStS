05c7e91f45825a17cc8f0fff0fcb9b9e5afafcdf
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-16 23:43:13 +0300

    Moved flow application to afterdamagegiven

--- PicklerFrigilCode/Cards/Ancient/BlizzardKick.cs
+++ PicklerFrigilCode/Cards/Ancient/BlizzardKick.cs
@@ -1,9 +1,11 @@
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 using PicklerFrigil.PicklerFrigilCode.Character;
 using PicklerFrigil.PicklerFrigilCode.Powers;
@@ -32,15 +34,17 @@ public class BlizzardKick() : PicklerFrigilCard(1,
     
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay play)
     {
-        await CommonActions.CardAttack(this, play.Target).WithHitCount(DynamicVars["Repeat"].IntValue).Execute(choiceContext);
-        for (int i = 0; i < DynamicVars["Repeat"].BaseValue; i++)
-        {
-            
-            await PowerCmd.Apply<FlowPower>(choiceContext, Owner.Creature, DynamicVars["FlowPower"].BaseValue,
-                Owner.Creature, this);
-        }
+        await CommonActions.CardAttack(this, play).WithHitCount(DynamicVars["Repeat"].IntValue).Execute(choiceContext);
     }
 
+    public override async Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer, DamageResult result, ValueProp props,
+        Creature target, CardModel? cardSource)
+    {
+        if (cardSource != this)
+            return;
+        await PowerCmd.Apply<FlowPower>(choiceContext, Owner.Creature, DynamicVars["FlowPower"].BaseValue, Owner.Creature, this);
+    }
+    
     protected override void OnUpgrade()
     {
         DynamicVars.Damage.UpgradeValueBy(1m);
