b0842eeb2b734c690624609c4f5ecbb30cbc1f32
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-23 23:54:02 +0300

    Added Gemstone hovertip

--- PicklerFrigilCode/Cards/Power/Glitterseed.cs
+++ PicklerFrigilCode/Cards/Power/Glitterseed.cs
@@ -1,6 +1,7 @@
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Relics;
 using PicklerFrigil.PicklerFrigilCode.Cards;
@@ -13,6 +14,14 @@ public class Glitterseed() : PicklerFrigilCard(2,
     CardType.Power, CardRarity.Rare,
     TargetType.Self)
 {
+    protected override IEnumerable<IHoverTip> ExtraHoverTips
+    {
+        get
+        {
+            yield return HoverTipFactory.FromKeyword(GemstoneKeyword);
+        }
+    }
+    
     protected override IEnumerable<DynamicVar> CanonicalVars => [
         new PowerVar<GlitterseedPower>(2M)
     ];
