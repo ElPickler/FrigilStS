972ffe2c0eec114057b0969f6c6a7d4e89508002
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-26 13:26:01 +0300

    Now gives block, no longer reduces energy on upgrade

--- PicklerFrigilCode/Cards/Skill/Repurpose.cs
+++ PicklerFrigilCode/Cards/Skill/Repurpose.cs
@@ -1,7 +1,10 @@
+using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
 using PicklerFrigil.PicklerFrigilCode.Cards.Special;
 using PicklerFrigil.PicklerFrigilCode.Powers;
 
@@ -20,15 +23,18 @@ public class Repurpose() : PicklerFrigilCard(1,
         }
     }
     
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(8, ValueProp.Move)];
+    
     protected override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
+        await CommonActions.CardBlock(this, play);
         await PowerCmd.Apply<RepurposePower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
     {
-        EnergyCost.UpgradeBy(-1);
+        DynamicVars.Block.UpgradeValueBy(3);
     }
 }
