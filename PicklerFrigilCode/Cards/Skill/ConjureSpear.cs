b12e8bfeaf852aa6b89902be12e768beaf8b9061
El Pickler <119024007+ElPickler@users.noreply.github.com>
2026-08-29 17:01:41 +0300

    Slightly more accumulate, now copies enchantments to the cryospear

--- PicklerFrigilCode/Cards/Skill/ConjureSpear.cs
+++ PicklerFrigilCode/Cards/Skill/ConjureSpear.cs
@@ -1,9 +1,10 @@
-using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.GameInfo.Objects;
 using PicklerFrigil.PicklerFrigilCode.Cards.Special;
 using PicklerFrigil.PicklerFrigilCode.Commands;
 
@@ -16,7 +17,7 @@ public class ConjureSpear() : PicklerFrigilCard(1
     TargetType.Self)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [
-        new DynamicVar("Accumulate", 13M)
+        new DynamicVar("Accumulate", 14M)
     ];
     
     protected override IEnumerable<IHoverTip> ExtraHoverTips
@@ -33,10 +34,21 @@ public class ConjureSpear() : PicklerFrigilCard(1
         CardPlay play)
     {
         await AccumulateCmd.Accumulate(choiceContext, DynamicVars["Accumulate"].BaseValue, Owner, this);
+
+        if (Enchantment == null)
+            return;
+        
+        List<Cryospear> spears = AccumulateCmd.GetCryospears(Owner, false, true).ToList();
+        foreach (Cryospear spear in spears)
+        {
+            if (spear.Enchantment != null) continue;
+            EnchantmentModel enchantment = Enchantment.CanonicalInstance.ToMutable();
+            CardCmd.Enchant(enchantment, spear, Enchantment.Amount);
+        }
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars["Accumulate"].UpgradeValueBy(5m);
+        DynamicVars["Accumulate"].UpgradeValueBy(4m);
     }
 }
